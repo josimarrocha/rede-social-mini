83f435d205e8860261f8dcc41d40c22b7a5c6e2c
Josimar <rocha_josimar@yahoo.com.br>
2019-08-05 20:26:40 -0300

    Create config styles

---
+++ src/styles/index.js
@@ -0,0 +1,10 @@
+export default {
+  container: '1240px',
+  colorPrimary: '#E38073',
+  colorPrimaryDisabled: '#E38073',
+  colorWhite: '#f8f8f8',
+  colorBorder: 'rgba(102, 102, 102, 0.2)',
+  fontSmall: '0.875em',
+  fontSmallSmall: '0.687em',
+  guther20: '20px',
+}

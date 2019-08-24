48da6f9a3a31ba4d5d19a2df221a1e1a4d25c00c
Josimar <rocha_josimar@yahoo.com.br>
2019-08-23 23:24:05 -0300

    Update reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,16 @@
 import { combineReducers } from 'redux'
 import initialDataProfile from './posts'
 import userInfo from './userInfo'
+import friendsInfo from './friends'
+import search from './search'
+import comments from './comments'
+import socket from './socket-io'
 
 export default combineReducers({
   initialDataProfile,
-  userInfo
+  userInfo,
+  friendsInfo,
+  search,
+  comments,
+  socket
 })

2852527fc80a1d9a181c6524193abace6612a1fb
Josimar <rocha_josimar@yahoo.com.br>
2019-08-23 23:14:16 -0300

    Create component sendComment

---
+++ src/components/Comments/SendComment/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { SendCommentContainer } from './styles'
+
+const SendComment = ({ textareaRef, addComment, imageProfile, isAnswer, commentId }) => {
+  return (
+    <SendCommentContainer answer={isAnswer}>
+      <div className="img-user-logged">
+        {imageProfile
+          ? <img className='img' src={`http://localhost:3333/imageProfile/${imageProfile}`} alt="" />
+          : <img src={`images/user@50.png`} alt="" />
+        }
+      </div>
+      <form action="">
+        <textarea
+          ref={textareaRef} rows="2" cols="50"
+          placeholder='Escreva um comentário'
+          onKeyPress={(e) => addComment(e, { isAnswer, commentId })}
+          onInput={(e) => {
+            if (textareaRef.current.scrollHeight > textareaRef.current.offsetHeight) textareaRef.current.rows += 1
+          }} />
+      </form>
+    </SendCommentContainer>
+  )
+}
+
+export default SendComment
---
+++ src/components/Comments/SendComment/styles.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const SendCommentContainer = styled.div`
+  width: 100%;
+  display: flex;
+  
+  .img-user-logged{
+    flex: 1;
+    margin-right:  ${props => props.answer ? '-6px' : '3px'};
+
+    img{
+      max-width: ${props => props.answer ? '35px' : '100%'};
+      max-height: ${props => props.answer ? '35px' : '100%'}
+    }
+  }
+  form{
+    flex: 8;
+    textarea{
+      border-radius: 5px;
+      border:none;
+      border: 1px solid #ddd;
+      width: 100%;
+      min-height: 33px;
+      resize: vertical;
+      padding: 5px;
+    }
+  }
+`

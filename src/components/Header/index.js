740fb109d8356fd270ae59e0a885dc12271ce708
Josimar <rocha_josimar@yahoo.com.br>
2019-08-23 23:15:12 -0300

    Update header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import PendingFriends from '../UserInfo/PendingFriends'
+import MenuMobile from '../MenuMobile'
+import InputSearch from '../InputSearch'
+import { friendsPendingIo } from '../../reducers/friends/actionsCreators'
 import { HeaderContainer } from './styles'
+import styles from '../../styles'
 
-const Header = () => {
+const Header = ({ pendingFriends, userInfo, socket, friendsPendingIo }) => {
+  const [isModalFriendsPending, setIsModalFriendsPending] = useState(false)
+  const [isMenuMobile, setIsMMenuMobile] = useState(true)
+
+  useEffect(() => {
+    socket.friends && socket.friends.on('pendingFriends', (data) => {
+      friendsPendingIo(data)
+    })
+    if (window.innerWidth <= styles.containerSmall) {
+      setIsMMenuMobile(true)
+    } else {
+      setIsMMenuMobile(false)
+    }
+  }, [])
+
+  window.addEventListener('resize', function (e) {
+    if (e.path[0].innerWidth <= styles.containerSmall) {
+      setIsMMenuMobile(true)
+    }
+    else {
+      setIsMMenuMobile(false)
+    }
+  })
+
   return (
     <HeaderContainer>
       <div className="header-content">
-        <div className="header-logo">
-          <h2>Midia</h2>
+        <div>
+          <div className="header-logo">
+            <Link to='/'><h2>Midia</h2></Link>
+          </div>
+          <span onClick={() => setIsModalFriendsPending(!isModalFriendsPending)}>
+            {isModalFriendsPending &&
+              <PendingFriends
+                setIsModalFriendsPending={setIsModalFriendsPending}
+              />
+            }
+            <i className="fas fa-user-friends">
+              {!!pendingFriends.length && <b>{pendingFriends.filter(item => item.friend_id !== userInfo.id).length}</b>}
+            </i>
+          </span>
         </div>
-        <form action="">
-          <input type="text" className='form-input' name="search" />
-        </form>
+        {!isMenuMobile ? <InputSearch /> : <MenuMobile />}
       </div>
     </HeaderContainer>
   )
 }
 
-export default Header
+const mapStateToProps = state => ({
+  pendingFriends: state.friendsInfo.friendsPending,
+  userInfo: state.userInfo,
+  socket: state.socket
+})
+
+export default connect(mapStateToProps, { friendsPendingIo })(Header)
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -7,24 +7,46 @@ export const HeaderContainer = styled.header`
   width: 100%;
   top: 0;
   box-shadow: 1px 1px 5px #999;
+  z-index:1;
 
   .header-content{
     max-width: ${styles.container};
     height: 40px;
     display: flex;
     margin: 0 auto;
-    justify-content: space-between;
     align-items: center;
     color: #f8f8f8;
     padding: 0 10px;
+    position: relative;
 
-    .form-input{
-      border: none;
-      border-radius: 4px;
-      height: 25px;
-      outline: none;
-      padding-left: 5px;
-      width: 20rem;
+    div{
+      display: flex;
+      align-items: center;
+
+      .header-logo{
+        margin-right: 30px;
+
+        a{
+          color: ${styles.colorWhite};
+        }
+      }
+      span i{
+        font-size:20px;
+        position: relative;
+        text-align:center;
+        cursor: pointer;
+      }
+      span b{
+        padding-top: 2px;
+        position: absolute;
+        background: #333;
+        border-radius: 50%;
+        width: 18px;
+        color: #f8f8f8;
+        height: 18px;
+        font-size: 13px;
+        top: -5px;
+      }
     }
   }
 `

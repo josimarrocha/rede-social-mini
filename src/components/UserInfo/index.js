e69a453860b34b8ddedefb5c451002fda5fec39f
Josimar <rocha_josimar@yahoo.com.br>
2019-08-23 23:17:57 -0300

    Update component userInfo

--- src/components/UserInfo/index.js
+++ src/components/UserInfo/index.js
@@ -1,37 +1,186 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
+import api from '../../config/api'
+import { updateNameUser, updateDescription } from '../../reducers/userInfo/actionsCreators'
+import { loadingProfile, friendsPendingIo } from '../../reducers/friends/actionsCreators'
 import Friends from './Friends'
-import { UserAccountContainer, UserInfoContainer, UserImg, UserData } from './styles'
+import UserAccountConfig from './UserAccountConfig'
+import { UserAccountContainer, UserInfoContainer, UserImg, UserData, UserImgbackground } from './styles'
 
-const UserInfo = ({ userInfo }) => {
+const UserInfo = ({ userInfo, profile, updateNameUser, visitProfile, loadingProfile, updateDescription, socket, friendsPendingIo }) => {
+  const [isMenuConfigUser, setIsMenuConfigUser] = useState(false)
+  const [isUpdateNameUser, setIsUpdateNameUser] = useState(false)
+  const [isUpdateDescription, setIsUpdateDescription] = useState(false)
+  const [nameInput, setNameInput] = useState('')
+  const [descriptionText, setDescriptionText] = useState('')
+  const [profileInfo, setProfileInfo] = useState({})
+
+  useEffect(() => {
+    if (visitProfile) {
+      setProfileInfo(profile)
+    } else {
+      setProfileInfo(userInfo)
+    }
+    setNameInput(userInfo.username)
+  }, [userInfo, profile])
+
+  const updateName = () => {
+    if (nameInput !== userInfo.username) {
+      updateNameUser(nameInput)
+      setIsUpdateNameUser(false)
+    }
+    setIsUpdateNameUser(false)
+  }
+  const removeProfile = async id => {
+    await api.delete(`/profile/friend/delete/${id}`)
+    await loadingProfile(id)
+  }
+
+  const descriptionUpdate = () => {
+    if (descriptionText !== userInfo.description) {
+      updateDescription(descriptionText)
+      setIsUpdateDescription(false)
+    }
+    setIsUpdateDescription(false)
+  }
+
+  const addProfile = async (id) => {
+    await api.get(`/profile/friend/newFriend/${id}`)
+    await loadingProfile(id)
+    socket.friends.emit('pendingFriends', id)
+  }
+
   return (
     <UserAccountContainer>
       <UserInfoContainer>
+        {!visitProfile &&
+          <div className="menu-user-config"
+            onClick={() => setIsMenuConfigUser(!isMenuConfigUser)}>
+            <i className="fas fa-ellipsis-v"></i>
+          </div>
+        }
+        <UserImgbackground>
+        </UserImgbackground>
+        {isMenuConfigUser &&
+          <UserAccountConfig
+            setIsMenuConfigUser={setIsMenuConfigUser}
+            setIsUpdateNameUser={setIsUpdateNameUser}
+            setIsUpdateDescription={setIsUpdateDescription}
+          />
+        }
+
         <UserImg>
-          <img src={`http://localhost:3333/imageProfile/${userInfo.image_profile}`} alt="" />
+          {profileInfo.image_profile
+            ? <img src={`http://localhost:3333/imageProfile/${profileInfo.image_profile}`} alt="" />
+            : <img src={`http://localhost:3000/images/user@150.png`} alt="" />
+          }
         </UserImg>
         <UserData>
           <div className="user-info-username">
-            <h2>{userInfo.username}</h2>
+            {!isUpdateNameUser ? <h3>{profileInfo.name}</h3>
+              : <div>
+                <input
+                  type="text"
+                  defaultValue={userInfo.name}
+                  onChange={({ target: { value } }) => setNameInput(value)} />
+                <div className="actions">
+                  <button onClick={updateName}>
+                    <i className="fas fa-check-circle" />Alterar
+                  </button>
+                  <button onClick={() => setIsUpdateNameUser(false)}>
+                    <i className="fas fa-window-close" />Cancelar
+                  </button>
+                </div>
+              </div>
+            }
+
           </div>
           <div className="user-info-describe">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, porro vel sit molestias explicabo enim, voluptatum et pra</p>
+            <p>
+              {userInfo.description
+                ? userInfo.description
+                : !isUpdateDescription && <>
+                  Nenhuma descrição
+                    <span onClick={() => setIsUpdateDescription(true)}>
+                    <i className="far fa-edit"></i>Adicionar
+                    </span>
+                </>
+              }
+            </p>
+            {isUpdateDescription &&
+              <>
+                <textarea
+                  name=""
+                  cols="30"
+                  rows="5"
+                  placeholder='Digite uma descrição para seu perfil'
+                  defaultValue={userInfo.description}
+                  onChange={({ target: { value } }) => setDescriptionText(value)}
+                />
+                <div className="actions">
+                  <button onClick={descriptionUpdate}>
+                    <i className="fas fa-check-circle" />Alterar
+                  </button>
+                  <button onClick={() => setIsUpdateDescription(false)}>
+                    <i className="fas fa-window-close" />Cancelar
+                  </button>
+                </div>
+              </>
+            }
           </div>
-          <div className="user-info-email">
+          {!visitProfile && <div className="user-info-email">
             <p>{userInfo.email}</p>
-          </div>
+          </div>}
           <div className="user-info-local">
             <p>Brasil, São Paulo, SP</p>
           </div>
+
+          <div className='profile-status'>
+            {profileInfo.isFriend === true &&
+              <>
+                <span className='profile-status-friend'>
+                  <i className="fas fa-check-circle" /> Amigos
+                </span>
+                <a href="/" className='profile-rm' onClick={(e) => {
+                  e.preventDefault()
+                  removeProfile(profileInfo.id)
+                }}>Desfazer amizade</a>
+              </>
+            }
+
+            {profileInfo.isFriend === null &&
+              <div className='profile-add'>
+                <a href="/" onClick={(e) => {
+                  e.preventDefault()
+                  addProfile(profileInfo.id)
+                }}>
+                  <i className="fas fa-user-plus" />Adicionar aos amigos
+                </a>
+              </div>
+            }
+
+            {profileInfo.isFriend === false &&
+              <div className='profile-add-cancel'>
+                <span href="/">
+                  Aguardando solicitação...
+              </span>
+                <a href="/">
+                  Cancelar solicitação
+              </a>
+              </div>
+            }
+          </div>
         </UserData>
       </UserInfoContainer>
-      <Friends />
+      <Friends visitProfile={visitProfile} />
     </UserAccountContainer>
   )
 }
 
 const mapStateToProps = state => ({
-  userInfo: state.userInfo
+  userInfo: state.userInfo,
+  profile: state.friendsInfo.friendProfile,
+  socket: state.socket
 })
 
-export default connect(mapStateToProps)(UserInfo)
+export default connect(mapStateToProps, { updateNameUser, loadingProfile, updateDescription, friendsPendingIo })(UserInfo)
--- src/components/UserInfo/styles.js
+++ src/components/UserInfo/styles.js
@@ -5,6 +5,12 @@ export const UserAccountContainer = styled.section`
   max-width: 280px;
   width:100%;
   position: fixed;
+  z-index: 0;
+
+  @media (max-width: ${styles.containerMiddle}) {
+    position: relative;
+    max-width: 100%;
+  }
 `
 
 export const UserInfoContainer = styled.div`
@@ -13,35 +19,163 @@ export const UserInfoContainer = styled.div`
   border-radius: 4px;
   box-shadow: 1px 1px 15px #ddd;
   margin-bottom: 20px;
+  position: relative;
+
+  .menu-user-config{
+    position: absolute;
+    right: 0;
+    top: 170px;
+    cursor: pointer;
+    padding: 5px;
+  }
 `
 
 export const UserImg = styled.div`
   width: 100%;
-
+  position: relative;
+  z-index: 1;
   img{
+    max-width: 100px;
     border-radius: 50%;
-    margin: 0 auto;
+    margin: 50px auto 0 auto;
     display: block
   }
 `
 
+export const UserImgbackground = styled.div`
+  width:100%;
+  height: 170px;
+  position:absolute;
+  top: 0;
+  left: 0;
+  background-image: url('http://localhost:3000/images/tarn-nguyen-4a52btspyY8-unsplash.jpg');
+  background-size:cover;
+  background-position: center;
+
+  @media (max-width: ${styles.containerMiddle}) {
+      background-position-y: -60px;
+  }
+
+`
+
 export const UserData = styled.div`
   text-align:center;
+  position: relative;
+  z-index: 1;
   margin-top:20px;
   font-size:${styles.fontSmall};
 
   .user-info-username{
     margin-bottom: 20px;
+    vertical-align: middle;
+
+    div{
+      display: flex;
+      flex-wrap: wrap;
+      input{
+        height: 30px;
+        width: 80%;
+        flex: 1 1 100%;
+      }
+    }
   }
   .user-info-describe p{
-    letter-spacing: 0.5px;    
+    letter-spacing: 0.5px; 
+    font-style: italic;
+    @media (max-width: ${styles.containerMiddle}) {
+      max-width: 350px;
+      margin: 0 auto;
+    }
+    span{
+      margin-left: 8px;
+      cursor: pointer;
+    }
+  }
+
+  .user-info-username .actions, .user-info-describe .actions{
+    display: flex;
+    flex: 1 1 100%;
+    justify-content: space-around;
+    align-items: center;
+    margin-top: 10px;
+    button{
+      padding: 4px 5px;
+    }
+    i{
+      color: ${styles.colorPrimary};
+      font-size: 20px;
+      padding: 0px 8px;
+    } 
   }
   .user-info-email{
     margin-top: 20px;
-    font-weight:500;
+    font-weight:600;
     font-style: italic;
   }
   .user-info-local{
     margin-top: 20px;
   }
+
+
+  .profile-status-friend{
+    display: flex;
+    justify-content: center;
+    align-items:center;
+    border: 1px solid #999;
+    padding: 5px 0;
+    margin-top: 8px;
+    color: #333;
+    font-weight:600;
+    cursor: default;
+
+    i{
+      margin-right:8px;
+      font-size:20px;
+      color: green;
+    }
+  }
+
+  .profile-rm{
+    display: block;
+    font-size: ${styles.fontSmall};
+    margin-top: 5px;
+    color: #333;
+    text-decoration: underline;
+  }
+
+  .profile-add{
+    a{
+      margin-top: 5px;
+      display: block;
+      color: ${styles.colorWhite};
+      border: 1px solid #ccc;
+      padding: 7px 0;
+      background: ${styles.colorPrimary};
+    }
+    i{
+      margin-right: 8px;
+    }
+  }
+  .profile-add-cancel{
+    span{
+      margin-top: 5px;
+      display: block;
+      color: ${styles.colorWhite};
+      border: 1px solid #ccc;
+      padding: 7px 0;
+      background: ${styles.colorPrimary};
+    }
+    a{
+      margin-top: 5px;
+      display: block;
+      color: ${styles.colorWhite};
+      border: 1px solid #ccc;
+      padding: 7px 0;
+      background: ${styles.colorPrimary};
+    }
+    i{
+      margin-right: 8px;
+    }
+  } 
+  
 `

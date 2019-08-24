7f70057513c4b0468d6e12c7d5d76a34e129d073
Josimar <rocha_josimar@yahoo.com.br>
2019-08-23 23:26:11 -0300

    Update reducer userInfo

--- src/reducers/userInfo/actionsCreators.js
+++ src/reducers/userInfo/actionsCreators.js
@@ -1,17 +1,17 @@
 import api from '../../config/api'
 export const LOGIN_USER = 'user:LOGIN_USER'
+export const UPDATE_IMAGE_USER = 'user:UPDATE_IMAGE_USER'
+export const UPDATE_NAME_USER = 'user:UPDATE_NAME_USER'
+export const LOADING_PROFILE = 'user:LOADING_PROFILE'
+export const UPDATE_DESCRIPTION_USER = 'user:UPDATE_DESCRIPTION_USER'
 
-
-export const loginUser = () => async dispatch => {
-  const isToken = localStorage.getItem('@tokenmidiasocial')
-
-  // if (!isToken) {
-  const { data: { token, username, image_profile, image_profile_mini, id, email } } =
-    await api.post('auth/login', {
-      email: 'jo@yahoo.com',
-      password: '123456'
-    })
-  localStorage.setItem('@tokenmidiasocial', token)
+export const loginUser = ({ token, username, image_profile, image_profile_mini, id, email }) => async dispatch => {
+  localStorage.setItem('@midiasocial@', JSON.stringify({
+    token,
+    id,
+    email,
+    username
+  }))
 
   dispatch({
     type: LOGIN_USER,
@@ -20,8 +20,42 @@ export const loginUser = () => async dispatch => {
       username,
       email,
       image_profile,
-      image_profile_mini
+      image_profile_mini,
+      token
     }
   })
-  // }
+}
+
+export const updateImageUser = (image) => async dispatch => {
+  const { data: { newImage } } = await api.put(`profile/updateImage`, image)
+  dispatch({
+    type: UPDATE_IMAGE_USER,
+    payload: newImage
+  })
+}
+
+export const updateNameUser = (nameUser) => async dispatch => {
+  const { data: { name } } = await api.put(`profile/updateName`, { nameUser })
+  dispatch({
+    type: UPDATE_NAME_USER,
+    payload: name
+  })
+}
+
+export const updateDescription = (userDescription) => async dispatch => {
+  const { data: { description } } = await api.put('profile/updateDescription', { description: userDescription })
+
+  dispatch({
+    type: UPDATE_DESCRIPTION_USER,
+    payload: description
+  })
+
+}
+
+export const loadingProfile = (id, token) => async dispatch => {
+  const { data: profileFriend } = await api.get(`userProfile/${id}`)
+  dispatch({
+    type: LOADING_PROFILE,
+    payload: profileFriend[0]
+  })
 }
--- src/reducers/userInfo/index.js
+++ src/reducers/userInfo/index.js
@@ -1,4 +1,4 @@
-import { LOGIN_USER } from './actionsCreators'
+import { LOGIN_USER, UPDATE_IMAGE_USER, UPDATE_NAME_USER, LOADING_PROFILE, UPDATE_DESCRIPTION_USER } from './actionsCreators'
 
 const userInfo = (state = {}, action) => {
   switch (action.type) {
@@ -6,6 +6,26 @@ const userInfo = (state = {}, action) => {
       return {
         ...state,
         ...action.payload
+      }
+    case LOADING_PROFILE: {
+      return {
+        ...action.payload
+      }
+    }
+    case UPDATE_IMAGE_USER:
+      return {
+        ...state,
+        image_profile: action.payload
+      }
+    case UPDATE_NAME_USER:
+      return {
+        ...state,
+        name: action.payload
+      }
+    case UPDATE_DESCRIPTION_USER:
+      return {
+        ...state,
+        description: action.payload
       }
     default:
       return state

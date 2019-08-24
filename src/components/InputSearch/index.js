603d72126e8467918adac262a2528887b1aab349
Josimar <rocha_josimar@yahoo.com.br>
2019-08-23 23:15:38 -0300

    Create component inputSearch

---
+++ src/components/InputSearch/index.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { searchProfile } from '../../reducers/search/actionsCreators'
+import { FormConatiner } from './styles'
+
+const InputSearch = ({ search, searchProfile, mobile }) => {
+  const [isEmptyInput, setIsEmptyInput] = useState(false)
+  const [inputSearch, setInputSearch] = useState('')
+
+  const searchUser = (e) => {
+    setInputSearch(e.target.value)
+    if (e.target.value) {
+      const user = JSON.parse(localStorage.getItem('@midiasocial@'))
+      searchProfile(e.target.value, user.token)
+      setIsEmptyInput(true)
+      return true
+    }
+    setIsEmptyInput(false)
+  }
+
+  return (
+    <FormConatiner mobile={mobile}>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          autoComplete="off"
+          onChange={searchUser}
+          value={inputSearch}
+          className='form-input' name="search" placeholder='Pesquisar usuário' />
+      </form>
+      {isEmptyInput && <div className="result-list">
+        <ul>
+          {search.map(item => (
+            <Link to={`profile/${item.id}`} key={`profile:${item.id}`}>
+              <li onClick={() => {
+                setIsEmptyInput(false)
+                setInputSearch('')
+              }}>
+                <span>
+                  <img src={`http://localhost:3333/imageProfile/${item.image_profile_mini}`} alt="" />
+                </span>
+                <b>{item.username}</b>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </div>}
+    </FormConatiner>
+  )
+}
+
+const mapStateToProps = state => ({
+  search: state.search
+})
+
+export default connect(mapStateToProps, { searchProfile })(InputSearch)
---
+++ src/components/InputSearch/styles.js
@@ -0,0 +1,72 @@
+import styled from 'styled-components'
+import styles from '../../styles'
+
+export const FormConatiner = styled.div`
+  position: relative;
+  form {
+    position: relative;
+    margin-left: 15rem;
+    flex: 2;
+
+    @media (max-width: ${styles.containerMiddle}) {
+      margin-left: 10rem;
+    }
+
+    @media (max-width: ${styles.containerSmall + 'px'}) {
+      margin-left: 0rem
+      }
+    
+    .form-input{
+      border: none;
+      border-radius: 4px;
+      height: 25px;
+      outline: none;
+      padding-left: 5px;
+      width: 20rem;
+      font-size: ${styles.fontSmall}; 
+      @media (max-width: ${styles.containerSmall + 'px'}) {
+        width: 20rem;
+        height: 35px;
+        border-bottom: 1px solid #aaa;
+
+      }
+    }
+  }
+
+    .result-list{
+      background: ${styles.colorWhite};
+      right: 0;
+      top: 26px;
+      position: absolute;
+      width: 20rem;
+      box-shadow: 0px 1px 5px #666;  
+      ul{
+        width: 100%;
+      }
+
+      li{
+        display: flex;
+        color: #333;
+        padding: 3px 5px 3px 5px;
+        border-bottom: 1px solid #ddd;
+        cursor: pointer;
+
+        &:hover{
+          background: #ddd;
+        }
+
+        span{
+          width: 33px;
+          margin-right: 10px;
+          img{
+            width: 100%;
+            max-width: 100%;
+          }
+        }
+
+        b{
+          font-size: ${styles.fontSmall}
+        }
+      }
+    }
+`

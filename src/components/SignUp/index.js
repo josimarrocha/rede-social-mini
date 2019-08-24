86dbee84cba70e74e48ddb473d5ae81e7c111028
Josimar <rocha_josimar@yahoo.com.br>
2019-08-23 23:16:56 -0300

    Create component signUp

---
+++ src/components/SignUp/index.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import FormSignUp from '../Form'
+import { Container } from './styles'
+import api from '../../config/api'
+
+const SignUp = ({ setIsLogin, isLogin }) => {
+  const [error, setError] = useState('')
+  const [success, setSuccess] = useState('')
+
+  const signUp = async (e) => {
+    e.preventDefault()
+    const { name, email, password, confirmpassword } = e.target
+    if (name.value && email.value && password.value && confirmpassword.value) {
+      if (password.value === confirmpassword.value) {
+        await api.post('auth/register', {
+          name: name.value,
+          email: email.value,
+          password: password.value
+        })
+        setSuccess('Cadastro realizado com sucesso!')
+
+        setTimeout(() => {
+          setIsLogin(true)
+        }, 1200)
+        return true
+      }
+      return setError('Senhas não correspondem!')
+    }
+    return setError('Todos os campos são obrigatórios!')
+
+  }
+
+  return (
+    <Container>
+      <FormSignUp
+        submit={signUp}
+        error={error}
+        success={success}
+        signUp={true}
+        setIsLogin={setIsLogin}
+        isLogin={isLogin}
+      />
+    </Container>
+  )
+}
+
+export default SignUp
---
+++ src/components/SignUp/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  flex: 1;
+  width: 100%;
+  padding: 0 10px;
+  position: relative;
+`

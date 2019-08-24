ef04668e060d260c9fd3a4d228567df89e065f6f
Josimar <rocha_josimar@yahoo.com.br>
2019-08-23 23:12:52 -0300

    Update component app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'
+import { connect } from 'react-redux'
 import Routes from './routes'
 import Header from './components/Header'
+import Auth from './pages/Auth'
+import { initSocket } from './reducers/socket-io/actionsCreators'
 
-function App() {
+function App({ initSocket }) {
+  const [isAuth, setIsAuth] = useState(null)
+
+  useEffect(() => {
+    const isToken = JSON.parse(localStorage.getItem('@midiasocial@'))
+    if (isToken) {
+      initSocket()
+      setIsAuth(false)
+    } else {
+      setIsAuth(true)
+    }
+  }, [])
   return (
     <Router>
-      <Header />
-      <Routes />
+      {isAuth === false &&
+        <>
+          <Header />
+          <Routes />
+        </>
+      }
+
+      {isAuth === true && <Auth setIsAuth={setIsAuth} />}
     </Router>
   );
 }
 
-export default App;
+export default connect(null, { initSocket })(App);

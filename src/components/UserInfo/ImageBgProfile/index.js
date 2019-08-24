96366863f3d53311d2d0358b6cfc74d0440d008a
Josimar <rocha_josimar@yahoo.com.br>
2019-08-23 23:18:50 -0300

    Create component ImageBgProfile

---
+++ src/components/UserInfo/ImageBgProfile/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ContainerImage } from './styles'
+
+const ImageBgProfile = () => {
+  return (
+    <ContainerImage>
+      <div className="image-bg">
+        <img src="http://localhost:3000/images/tarn-nguyen-4a52btspyY8-unsplash.jpg" alt="" />
+      </div>
+    </ContainerImage>
+  )
+}
+
+export default ImageBgProfile
---
+++ src/components/UserInfo/ImageBgProfile/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const ContainerImage = styled.div`
+  margin-left: 300px;
+  width: calc(100% - 300px);
+  margin-bottom: 20px;
+  
+  .image-bg{
+    img{
+      width: 100%;
+      max-width: 100%;
+    }
+  }
+`

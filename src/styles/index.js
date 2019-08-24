63671001ddec0db8f176421eb2d08caf67f73a98
Josimar <rocha_josimar@yahoo.com.br>
2019-08-23 23:26:26 -0300

    Update styles global

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,10 +1,12 @@
 export default {
   container: '1240px',
+  containerMiddle: '990px',
+  containerSmall: 740,
   colorPrimary: '#E38073',
   colorPrimaryDisabled: '#E38073',
   colorWhite: '#f8f8f8',
   colorBorder: 'rgba(102, 102, 102, 0.2)',
   fontSmall: '0.875em',
-  fontSmallSmall: '0.687em',
+  fontSmallSmall: '0.717em',
   guther20: '20px',
 }

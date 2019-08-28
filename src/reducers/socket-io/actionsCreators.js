802b77b60043b25f4fda431c0a1f8a7139797043
Josimar <rocha_josimar@yahoo.com.br>
2019-08-28 19:02:13 -0300

    Change url socket

--- src/reducers/socket-io/actionsCreators.js
+++ src/reducers/socket-io/actionsCreators.js
@@ -3,7 +3,8 @@ import Ws from '@adonisjs/websocket-client'
 export const INIT_SOCKET = 'socket:INIT_SOCKET'
 
 export const initSocket = () => dispatch => {
-  const io = Ws('ws://localhost:3333')
+  const io = Ws('ws://midia-social.herokuapp.com')
+  // const io = Ws('ws://localhost:3333')
   const { token } = JSON.parse(localStorage.getItem('@midiasocial@'))
   io.withJwtToken(token)
     .connect()

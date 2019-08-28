465fc0690a9763d5cb0796ed220f49e5caad6de3
josimarrocha <rocha_josimar@yahoo.com.br>
2019-08-28 23:28:09 +0000

    Updates

---
+++ precache-manifest.be97c8035e258439cbc25577fb55b070.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e25e0cad5766e73905ac7314c9bc687",
+    "url": "/rede-social-mini/index.html"
+  },
+  {
+    "revision": "45f357ac9867c73d10fe",
+    "url": "/rede-social-mini/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "e56a2991985318e71c60",
+    "url": "/rede-social-mini/static/css/main.4affd967.chunk.css"
+  },
+  {
+    "revision": "45f357ac9867c73d10fe",
+    "url": "/rede-social-mini/static/js/2.881f82e0.chunk.js"
+  },
+  {
+    "revision": "e56a2991985318e71c60",
+    "url": "/rede-social-mini/static/js/main.a3094c0a.chunk.js"
+  },
+  {
+    "revision": "a083ab8411362b8482dc",
+    "url": "/rede-social-mini/static/js/runtime~main.409cf430.js"
+  }
+]);

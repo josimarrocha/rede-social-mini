1932331d24c0a480c30821ae4515a0485e89299b
Josimar <rocha_josimar@yahoo.com.br>
2019-08-28 19:00:41 -0300

    Change url api

--- src/config/api.js
+++ src/config/api.js
@@ -4,14 +4,17 @@ const getToken = () => {
   const token = JSON.parse(localStorage.getItem('@midiasocial@'))
   if (token) {
     return axios.create({
-      baseURL: 'http://localhost:3333',
+      // baseURL: 'http://localhost:3333',
+      baseURL: 'https://midia-social.herokuapp.com',
       headers: {
         'Authorization': `${`bearer ${token.token}`}`,
       },
     })
+    //https://midia-social.herokuapp.com
   } else {
     return axios.create({
-      baseURL: 'http://localhost:3333'
+      // baseURL: 'http://localhost:3333'
+      baseURL: 'https://midia-social.herokuapp.com'
     })
   }
 }

846f14aafdadc5ece6ee4ae48d814dd799b6ea4f
Josimar <rocha_josimar@yahoo.com.br>
2019-09-02 21:51:08 -0300

    Update func renderFriends

--- src/components/UserInfo/Friends/index.js
+++ src/components/UserInfo/Friends/index.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { FreindsContainer, Friend } from './styles'
 
-const Friends = ({ friends }) => {
+const Friends = ({ friends, userInfo }) => {
   useEffect(() => {
     renderFriends()
   }, [friends])
@@ -11,12 +11,12 @@ const Friends = ({ friends }) => {
   const renderFriends = () => {
     return (
       friends.map(friend => (
-        <Link to={`/${friend.username}/${friend.id}`} key={`friend:${friend.id}`}>
+        userInfo.id !== friend.id && <Link to={`/${friend.username}/${friend.id}`} key={`friend:${friend.id}`}>
           <Friend>
             <div className="friend-img">
               {friend.image_profile_mini
                 ? <img src={friend.image_profile_mini} alt="" />
-                : <img src={`/images/user@50.png`} alt="" />
+                : <img src={`/rede-social-mini/images/user@50.png`} alt="" />
               }
             </div>
             <div className="friend-name">
@@ -47,7 +47,8 @@ const Friends = ({ friends }) => {
 }
 
 const mapStateToProps = state => ({
-  friends: state.friendsInfo.friends
+  friends: state.friendsInfo.friends,
+  userInfo: state.userInfo
 })
 
 export default connect(mapStateToProps)(Friends)

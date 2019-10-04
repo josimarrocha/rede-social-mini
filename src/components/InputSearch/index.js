ab02dd3d4cea233ec8b685a77be5d8e579bdaa70
Josimar <rocha_josimar@yahoo.com.br>
2019-10-04 16:35:27 -0300

    Remove method renderListUsers component inputSearch

--- src/components/InputSearch/index.js
+++ src/components/InputSearch/index.js
@@ -1,66 +1,30 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import { connect } from 'react-redux'
-import pathImageDefault from '../../config/util'
-import { searchProfile } from '../../reducers/search/actionsCreators'
+import ListUser from './ListUser'
 import { FormConatiner } from './styles'
 
-const InputSearch = ({ search, searchProfile, inHeader, userMarkup }) => {
-  const [isEmptyInput, setIsEmptyInput] = useState(false)
+const InputSearch = ({ inHeader }) => {
   const [inputSearch, setInputSearch] = useState('')
-
-  const searchUser = (e) => {
-    setInputSearch(e.target.value)
-    if (e.target.value) {
-      const user = JSON.parse(localStorage.getItem('@midiasocial@'))
-      searchProfile(e.target.value, user.token)
-      setIsEmptyInput(true)
-      return true
-    }
-    setIsEmptyInput(false)
-  }
-
-  const renderListUsers = () => {
-    return search.map(item => (
-      <Action to={`/${item.username}/${item.id}`} key={`profile:${item.id}`}>
-        <li onClick={() => {
-          setIsEmptyInput(false)
-          setInputSearch('')
-        }}>
-          <span>
-            <img src={item.image_profile_mini
-              ? item.image_profile_mini :
-              `${pathImageDefault.pathImageDev}/user@50.png`} alt="" />
-          </span>
-          <b>{item.username}</b>
-        </li>
-      </Action>
-    ))
-  }
-
-  const Action = inHeader ? Link : 'span'
-
   return (
     <FormConatiner inHeader={inHeader}>
-      {inHeader && <form onSubmit={(e) => e.preventDefault()}>
-        <input
-          type="text"
-          autoComplete="off"
-          onChange={searchUser}
-          value={inputSearch}
-          className='form-input' name="search" placeholder='Pesquisar usuário' />
-      </form>}
-      <div className="result-list">
-        <ul>
-          {inHeader && isEmptyInput ? renderListUsers() : !inHeader && userMarkup ? renderListUsers() : ''}
-        </ul>
-      </div>
-    </FormConatiner>
+      {inHeader &&
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            type="text"
+            autoComplete="off"
+            onChange={({ target: { value } }) => setInputSearch(value)}
+            value={inputSearch}
+            className='form-input' name="search" placeholder='Pesquisar usuário' />
+          {inputSearch &&
+            <ListUser
+              inHeader={true}
+              link={true}
+              cleanInput={setInputSearch}
+              userSearch={inputSearch} />
+          }
+        </form>
+      }
+    </FormConatiner >
   )
 }
 
-const mapStateToProps = state => ({
-  search: state.search
-})
-
-export default connect(mapStateToProps, { searchProfile })(InputSearch)
+export default InputSearch

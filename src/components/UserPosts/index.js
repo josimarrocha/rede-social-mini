8c87ed2429e13c603161b031813ac7fec6e2e029
Josimar <rocha_josimar@yahoo.com.br>
2019-10-04 16:47:54 -0300

    Create method registerPostViews

--- src/components/UserPosts/index.js
+++ src/components/UserPosts/index.js
@@ -1,34 +1,58 @@
 import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
+import api from '../../config/api'
 import NewPost from './NewPost'
+import MessagesViewed from '../MessagesViewed'
 import Post from './Post'
 import { ContainerPosts } from './styles'
 
+let postsIds = {}
 const UserPosts = ({ posts, userInfo, profile, postsTimeline, isScroll, profileId, url }) => {
   const [isInitScrollEvent, setIsInitScrollEvent] = useState(true)
+  const [linePositionInScreen, setLinePositionInScreen] = useState()
   let scrollY = 0
+
   useEffect(() => {
+    setLinePositionInScreen(document.querySelector('.area').getBoundingClientRect().top)
     isScroll ? postsTimeline(1, profileId) : postsTimeline(url)
     return () => window.onscroll = null
   }, [])
 
-  const scroll = (e) => {
+  const registerPostViews = async () => {
+    let postss = document.querySelectorAll('.post-teste')
+    postss && await postss.forEach(async post => {
+      if (post.getBoundingClientRect().top < linePositionInScreen) {
+        let [postId, userId] = post.getAttribute('datatype').split(':')
+        if (!postsIds[postId] && userInfo.id !== +userId) {
+          await api.post('post/views', { postId })
+        }
+        postsIds = {
+          ...postsIds,
+          [postId]: true
+        }
+      }
+    })
+  }
+
+  const scroll = async (e) => {
+    isInitScrollEvent && registerPostViews()
     setIsInitScrollEvent(true)
     let pagey = posts.page + 1
     scrollY = e.currentTarget.scrollY + e.currentTarget.innerHeight
-    if (document.body.scrollHeight - scrollY < 600) {
+    if (document.body.scrollHeight - scrollY < 500) {
       isScroll && isInitScrollEvent && postsTimeline(pagey, profile.id)
       setIsInitScrollEvent(false)
     }
   }
-
   window.onscroll = scroll
 
   return (
-    <ContainerPosts>
+    <ContainerPosts >
+      <MessagesViewed />
       {userInfo.id === profile.id && <NewPost />}
       {posts.posts.map((post, i) => (
-        <Post key={`post:${post.id}${i}`}
+        <Post
+          key={`post:${post.id}${i}`}
           post={post}
         />
       ))}

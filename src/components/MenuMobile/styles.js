9890e7e6a71c9a5667ffc809fed3d4438f21f3e1
Josimar <rocha_josimar@yahoo.com.br>
2019-10-04 16:39:01 -0300

    Change styles menuMobile

--- src/components/MenuMobile/styles.js
+++ src/components/MenuMobile/styles.js
@@ -2,45 +2,48 @@ import styled from 'styled-components'
 import styles from '../../styles'
 
 export const MenuMobileContainer = styled.div`
-  flex: 2;
-  position: relative;
-  
+  position: absolute;
+  display: none;
+  width: 100%;
+  right: 10px;
+  @media (max-width: ${styles.containerSmall + 'px'}) {
+    display: block;
+  }
   .menu-content{
     display: none !important;
-    position: relative;
+    position: absolute;
+    width: 100%;
+    max-width: 200px;
 
     .icon-menu{
-      padding: 8px;
+      position: absolute;
+      right: 0px;
+      top:-10px;
+      padding:0 8px;
     }
 
     @media (max-width: ${styles.containerMiddle}) {
       display: block !important;
       position: absolute;
-      right: 10px;
+      right: 0px;
       font-size: 20px;
     }
     
     .menu-list{
-      display: flex;
-      flex-direction: column;
-      align-items: center;
       font-size: ${styles.fontSmall};
-      color: ${styles.colorWhite};
-      background: #fff;
+      color: #444;
       box-shadow: 0 2px 5px #666;
       position: absolute;
       z-index: 20;
-      top: 33px;
-      right: -15px;
+      top: 20px;
+      width: 100%;
     }  
     .menu-item{
-      background: ${styles.colorPrimary};
-      text-transform: uppercase;
-      font-weight: 600;
-      text-align: center;
-      border-bottom: 1px solid #ccc;
-      font-size: ${styles.fontSmall};
-      padding: 10px 0;
+      background: ${styles.colorWhite};
+      text-align: left;
+       border-bottom: 1px solid #ccc;
+      font-size: 16px;
+      padding: 8px 10px;
       width: 100%;
     }
   }

1d071515b6c3f88c9fef770106f31a7b67a6212c
Josimar <rocha_josimar@yahoo.com.br>
2019-10-04 16:45:08 -0300

    Create method removeUser; change path image

--- src/components/UserInfo/PendingFriends/index.js
+++ src/components/UserInfo/PendingFriends/index.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react'
+import api from '../../../config/api'
 import { connect } from 'react-redux'
 import { friendsPending, confirmFriend, showFriends, loadingProfile } from '../../../reducers/friends/actionsCreators'
 import { PendingFriendsConatiner, PendingFriendsContent } from './styles'
 
-const PendingFriends = ({ loadingProfile, profile, friendsPending, friendsInfo, confirmFriend, showFriends }) => {
+const PendingFriends = ({ profile, friendsPending, friendsInfo, confirmFriend, showFriends }) => {
   useEffect(() => {
     friendsPending()
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -13,7 +14,11 @@ const PendingFriends = ({ loadingProfile, profile, friendsPending, friendsInfo, 
     await confirmFriend(id)
     await friendsPending()
     await showFriends(profile.id)
-    loadingProfile(profile.id)
+    // loadingProfile(profile.id)
+  }
+  const removeUser = async (userId) => {
+    await api.delete(`/profile/friend/delete/${userId}`)
+    await friendsPending()
   }
 
   return (
@@ -27,16 +32,13 @@ const PendingFriends = ({ loadingProfile, profile, friendsPending, friendsInfo, 
           : friendsInfo.map(friendsPend => (
             <div className="pending-friends" key={`friendPending:${friendsPend.friend_id}`}>
               <div className="img-friend">
-                {friendsPend.image_profile_mini
-                  ? <img src={`http://localhost:3333/imageProfile/${friendsPend.image_profile_mini}`} alt="" />
-                  : <img src={`images/user@50.png`} alt="" />
-                }
+                <img src={friendsPend.image_profile_mini ? friendsPend.image_profile_mini : 'images/user@50.png'} alt="" />
               </div>
               <div className="pending-friend">
                 <p className="friend-name">{friendsPend.name}</p>
                 <div className="actions">
                   <button onClick={() => confirmFriendship(friendsPend.friend_id)}>Adicionar</button>
-                  <button>Remover</button>
+                  <button onClick={() => removeUser(friendsPend.friend_id)}>Remover</button>
                 </div>
               </div>
             </div>

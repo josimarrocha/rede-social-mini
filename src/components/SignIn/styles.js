e802476bf0f199e5ba9303e6bf566952f6399faa
Josimar <rocha_josimar@yahoo.com.br>
2019-10-16 00:01:32 -0300

    Change padding SignIn and SignUp

--- src/components/SignIn/styles.js
+++ src/components/SignIn/styles.js
@@ -3,5 +3,5 @@ import styled from 'styled-components'
 export const Container = styled.div`
   flex: 1;
   width: 100%;
-  padding: 0 10px;
+  padding: 0 20px;
 `
--- src/components/SignUp/styles.js
+++ src/components/SignUp/styles.js
@@ -3,6 +3,6 @@ import styled from 'styled-components'
 export const Container = styled.div`
   flex: 1;
   width: 100%;
-  padding: 0 10px;
+  padding: 0 20px;
   position: relative;
 `

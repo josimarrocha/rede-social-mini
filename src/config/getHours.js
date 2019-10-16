6fb0b0fde2fbf282af6c12346403e58db653b782
Josimar <rocha_josimar@yahoo.com.br>
2019-10-16 00:08:00 -0300

    Create method to format date

---
+++ src/config/getHours.js
@@ -0,0 +1,26 @@
+export default function (time, place) {
+  let hour = new Date(time).getHours()
+  let minutes = new Date(time).getMinutes()
+
+  minutes = minutes < 10 ? `0${minutes}` : minutes
+  hour = hour < 10 ? `0${hour}` : hour
+
+  let day = new Date(time).getDate()
+  let month = new Date(time).getMonth()
+  let year = new Date(time).getFullYear()
+  if (place === 'posts') {
+    return `${hour}:${minutes}`
+  }
+  if (day === new Date().getDate()) {
+    return `Hoje ${hour}:${minutes}`
+  }
+  if ((new Date().getDate() - day) > 1 || new Date().getMonth() + 1 > month + 1) {
+    return `${day < 10 ? `0${day}` : day}/${month < 10 ? `${month + 1}` : month}/${year}`
+  }
+  if ((new Date().getDate() - day) === 1) {
+    return `Ontem ${hour}:${minutes}`
+  }
+  // else {
+  //   return `Ontem ${hour}:${minutes}`
+  // }
+}

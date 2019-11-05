2ca12e862d1f50e9d17a8057cbd4840721374fb4
Josimar <rocha_josimar@yahoo.com.br>
2019-11-04 21:37:41 -0300

    Update api config

--- src/config/api.js
+++ src/config/api.js
@@ -1,21 +1,44 @@
 import axios from 'axios'
+import io from 'socket.io-client'
+
+export const urlRedeSocial = 'https://midia-social.herokuapp.com'
+// export const urlRedeSocial = 'http://localhost:3333'
+
+export const urlChat = 'https://api-chat-rede-social.herokuapp.com/'
+// export const urlChat = 'http://localhost:4444'
+
+export const socket = io(urlChat)
 
 const token = JSON.parse(localStorage.getItem('@midiasocial@'))
-const getToken = () => {
+const tokenChat = JSON.parse(localStorage.getItem('@chat-midiasocial@'))
+const getAPIRedeSocial = () => {
   if (token) {
     return axios.create({
-      // baseURL: 'http://localhost:3333',
-      baseURL: 'https://midia-social.herokuapp.com',
+      baseURL: urlRedeSocial,
       headers: {
-        'Authorization': `${`bearer ${token.token}`}`,
+        'Authorization': `bearer ${token.token}`
       },
     })
   } else {
     return axios.create({
-      // baseURL: 'http://localhost:3333'
-      baseURL: 'https://midia-social.herokuapp.com'
+      baseURL: urlRedeSocial
     })
   }
 }
 
-export default getToken()
+export const getAPIChat = () => {
+  if (tokenChat) {
+    return axios.create({
+      baseURL: urlChat,
+      headers: {
+        'Authorization': `bearer ${tokenChat.token}`,
+      },
+    })
+  } else {
+    return axios.create({
+      baseURL: urlChat
+    })
+  }
+}
+
+export default getAPIRedeSocial()

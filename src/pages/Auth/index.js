1db8437094529d5a7bb0dde19e7a30ce7dffc17e
Josimar <rocha_josimar@yahoo.com.br>
2019-11-04 21:40:33 -0300

    Update page Auth

--- src/pages/Auth/index.js
+++ src/pages/Auth/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Login from '../../components/SignIn'
 import SignUp from '../../components/SignUp'
+import imageCapa from '../../assets/images/capa-inicio.jpg'
 import { Container } from './styles'
 
 const token = JSON.parse(localStorage.getItem('@midiasocial@'))
@@ -19,7 +20,7 @@ const Auth = ({ history }) => {
         <div className="login-content">
           <div className="brand">
             <div className="preview">
-              <img src="images/capa-inicio.jpg" alt="" />
+              <img src={imageCapa} alt="" />
             </div>
           </div>
           {isLogin

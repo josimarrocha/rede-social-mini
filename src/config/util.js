7cf3d86b50cf28dfd0ddd1adc76fb52966d74868
Josimar <rocha_josimar@yahoo.com.br>
2019-11-04 21:40:21 -0300

    Change ultil

--- src/config/util.js
+++ src/config/util.js
@@ -1,10 +1,5 @@
 import Ws from '@adonisjs/websocket-client'
 
-export default {
-  pathImageProd: '/rede-social-mini/images',
-  pathImageDev: '/rede-social-mini/images'
-}
-
 export const userToken = JSON.parse(localStorage.getItem('@midiasocial@'))
 const io = Ws('wss://midia-social.herokuapp.com')
 // const io = Ws('ws://localhost:3333')
